ec383030c109c0addb08f51e456c737c22fd35ee
zhanweicao <chanweicao@gmail.com>
2024-08-04 19:35:50 -0700

    Assignment part DONE

--- Kanbas/Assignment/routes.js
+++ Kanbas/Assignment/routes.js
@@ -5,5 +5,33 @@ export default function AssignmentRoutes(app) {
         const assignments = db.assignments.filter((a) => a.course === cid);
         res.json(assignments);
     });
+
+    app.put("/api/courses/:cid/assignments/:aid", (req, res) => {
+      const { aid } = req.params;
+      const assignmentIndex = db.assignments.findIndex(
+          (a) => a._id === aid);
+      db.assignments[assignmentIndex] = {
+          ...db.assignments[assignmentIndex],
+          ...req.body
+      };``
+      res.sendStatus(204);
+  });
+
+  app.delete("/api/courses/:cid/assignments/:aid", (req, res) => {
+      const { aid } = req.params;
+      db.assignments = db.assignments.filter((a) => a._id !== aid);
+      res.sendStatus(200);
+  });
+
+  app.post("/api/courses/:cid/assignments/", (req, res) => {
+      const { cid } = req.params;
+      const newAssignment = {
+          ...req.body,
+          course: cid,
+          _id: new Date().getTime().toString(),
+      };
+      db.assignments.push(newAssignment);
+      res.send(newAssignment);
+  });
 }
 
--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -11,7 +11,7 @@ export default function ModuleRoutes(app) {
         res.sendStatus(204);
     });
 
-    app.delete("/api/modules/:mid", (req, res) => {
+   app.delete("/api/modules/:mid", (req, res) => {
         const { mid } = req.params;
         db.modules = db.modules.filter((m) => m._id !== mid);
         res.sendStatus(200);
@@ -29,7 +29,7 @@ export default function ModuleRoutes(app) {
     });
 
     app.get("/api/courses/:cid/modules", (req, res) => {
-        const { cid } = req.params;
+        const { cid }  = req.params;
         const modules = db.modules.filter((m) => m.course === cid);
         res.json(modules);
     });
