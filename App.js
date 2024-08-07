968ff2f7fa022d37e8bfade61c4c95b9685e1971
zhanweicao <chanweicao@gmail.com>
2024-08-06 17:16:45 -0700

    3.6.3 CANT get People Detail, starting 3.6.4

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from 'express';
 import mongoose from "mongoose";
 import Hello from "./Hello.js"
@@ -6,14 +7,16 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignment/routes.js';
+import UserRoutes from "./Kanbas/Users/routes.js";
 const app = express()
 app.use(cors());
-const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas"
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000)
